2bee07f85788b7b5ca66204409f061c1b3680630
bw1611 <bw1611@naver.com>
2023-04-05 11:49:10 +0900

    init commmit

---
+++ src/main/java/com/ll/gramgram/boundedContext/likeablePerson/repository/LikeablePersonRepository.java
@@ -0,0 +1,12 @@
+package com.ll.gramgram.boundedContext.likeablePerson.repository;
+
+import com.ll.gramgram.boundedContext.likeablePerson.entity.LikeablePerson;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface LikeablePersonRepository extends JpaRepository<LikeablePerson, Integer> {
+    List<LikeablePerson> findByFromInstaMemberId(Long fromInstaMemberId);
+
+}
